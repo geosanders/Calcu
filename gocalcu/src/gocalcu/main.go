86318937562df5da3721796a8917d22d0f560da1
Brad Peabody <bradpeabody@gmail.com>
2013-08-29 22:15:24 -0700

    removed a bunch of commented out code to clean things up

--- gocalcu/src/gocalcu/main.go
+++ gocalcu/src/gocalcu/main.go
@@ -55,29 +55,9 @@ func main() {
 		}
 	}
 
-	// if err != nil {
-	// 	panic("Error parsing args: " + err.Error())
-	// }
-	// fmt.Printf("Type of err: ", reflect.TypeOf(err) == flags.ErrHelp)
-	// return
-	// help error is fine, just exit
-	// if flags.Error(err).Type == flags.ErrHelp {
-	// 	return
-	// } else
-	// if err != nil { // catch other errors and report on them
-	// 	fmt.Printf("type:", flags.Error(err))
-	// 	panic("Error while parsing flags: " + err.Error())
-	// }
-
 	if len(args) > 0 {
 		panic("No idea what to do with extra args on the command line...")
 	}
-
-	// fmt.Printf("port: " + strconv.Itoa(int(opts.Port)) + "\n")
-
-	// for i := 0; i < len(args); i++ {
-	// 	fmt.Printf("arg: " + args[i] + "\n")
-	// }
 
 	// a channel for the events we read from the serial port
 	event_channel := make(chan string)
@@ -86,23 +66,11 @@ func main() {
 	// run IO separately
 	go func() {
 
-		// c := &serial.Config{Name: "/dev/tty.usbserial-A900YUBL", Baud: 115200}
-		// c := createSerialReader("/dev/tty.usbserial-A900YUBL")
+
 		s, err := createSerialReader(opts.SerialDevice)
 		if err != nil {
 			panic(err.Error())
 		}
-
-
-		// buf := make([]byte, 1)
-		// for true {
-		// 	_, err := s.Read(buf)
-		// 	if err != nil {
-		// 		panic(err.Error())
-		// 	}
-		// 	// fmt.Printf("read byte: %s\n", buf[0])
-		// 	fmt.Printf("%s", hex.Dump(buf))
-		// }
 
 
 		scanner := bufio.NewScanner(s)
@@ -117,7 +85,7 @@ func main() {
 				switch v.(type) {
 					case string: {
 						vs := v.(string)
-						fmt.Printf("string value: %s\n", vs)
+						// fmt.Printf("string value: %s\n", vs)
 						// push to channel
 						event_channel <- vs
 						break
@@ -128,27 +96,6 @@ func main() {
 
 
 
-		// var buffer bytes.Buffer
-
-		// buf := make([]byte, 4096)
-		// for true {
-		// 	n, err := s.Read(buf)
-		// 	if err != nil {
-		// 		panic(err.Error())
-		// 	}
-		// 	buffer.Write(buf[0:n])
-		// 	line, err2 := buffer.ReadString('\n')
-		// 	if err2 == nil || err2 == io.EOF {
-		// 		buffer.Reset()
-		// 	} else {
-		// 		fmt.Printf("Got data: %s\n", line)
-		// 		// trim the buffer down so it only contains what's left
-		// 	buffer = *bytes.NewBufferString(buffer.String())
-		// 	}
-
-		// }
-
-	    // c.Close() // hm, this only exists on windows...
 	}()
 
 
@@ -168,8 +115,6 @@ func main() {
 				break
 			}
 		}
-		// ws.Write
-		// io.Copy(ws, ws)
 	}))
 
 	fmt.Printf("Running\n")
@@ -182,17 +127,3 @@ func main() {
 }
 
 
-// func EchoServer(ws *websocket.Conn) {
-
-// 	// c := &serial.Config{Name: "/dev/tty.usbserial-A900YUBL-11", Baud: 115200}
-// 	// s, err := serial.OpenPort(c)
-// 	// if err != nil {
-// 	// 	msg := err.Error()
-// 	// 	b, _ := json.Marshal(m)
-// 	// 	ws.Write("{\"error\":"+b+"}")
-// 	// 	return
-// 	// }
-
-// 	// /dev/tty.usbserial-A900YUBL
-// 	io.Copy(ws, ws)
-// }
